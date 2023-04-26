92b5b71d5311ad726593fff5cb720f1acc25e76d
alexperezortuno <alexperezortuno@gmail.com>
2023-04-25 22:08:08 -0400

    ad more option for save audio

--- tts/__main__.py
+++ tts/__main__.py
@@ -4,6 +4,8 @@ import argparse
 import os.path
 import re
 import sys
+import sounddevice as sd
+import soundfile as sf
 from typing import Dict
 from gtts import gTTS
 from pydub import AudioSegment
@@ -11,35 +13,65 @@ from pydub.playback import play
 
 
 def start(data: Dict):
-    abs_path: str = os.path.dirname(os.path.abspath(__file__))
-    father_path: str = os.path.dirname(abs_path)
-    tts = gTTS(text=data['text'], lang=data['lang'], slow=data['slow'])
-    tts.save(f"{father_path}/{data['output']}")
-    file_path: str = f"{father_path}/{data['output']}"
-    _, format_ext = os.path.splitext(file_path)
-    print(file_path)
-    print(re.sub("[.]", "", format_ext))
+    try:
+        abs_path: str = os.path.dirname(os.path.abspath(__file__))
+        father_path: str = os.path.dirname(abs_path)
+        input_text: str = ""
 
-    if data['play']:
-        audio = AudioSegment.from_file(file_path, format=re.sub("[.]", "", format_ext))
-        play(audio)
+        if data['record']:
+            my_recording = sd.rec(
+                int(data['record_seconds'] * data['record_sample_rate']),
+                samplerate=data['record_sample_rate'],
+                channels=data['channels'],
+            )
+            sd.wait()
+            file_path: str = f"{father_path}/{data['output']}"
+            sf.write(file_path, my_recording, data['record_sample_rate'])
+
+        if data['text'] is None and data['input_file'] is None:
+            input_text = input("Enter text: ")
+
+        if data['text'] is not None:
+            input_text = data['text']
+
+        if data['input_file'] is not None:
+            with open(f"{father_path}/{data['input_file']}", "r") as f:
+                input_text = f.read()
+
+        tts = gTTS(text=input_text, lang=data['lang'], tld=data['tld'], slow=data['slow'])
+        tts.save(f"{father_path}/{data['output']}")
+
+        file_path: str = f"{father_path}/{data['output']}"
+        _, format_ext = os.path.splitext(file_path)
+
+        if data['play']:
+            audio = AudioSegment.from_file(file_path, format=re.sub("[.]", "", format_ext))
+            play(audio)
+    except Exception as ex:
+        raise ex
 
 
 if __name__ == '__main__':
     try:
-        parser = argparse.ArgumentParser(description="ZeroMQ workers",
+        parser = argparse.ArgumentParser(description="Text to speech",
                                          formatter_class=argparse.ArgumentDefaultsHelpFormatter)
         subparser = parser.add_subparsers(title='Script select', dest='script_type')
         parser.version = '0.0.0'
         parser.add_argument("-v", "--version", action="version")
         parser.add_argument("-t", "--text", type=str,
-                            default="Hello my name is Bark. I am a trained model. I hope you have a great day.",
+                            default=None,
                             help="Text to be turned into audio")
+        parser.add_argument("-i", "--input_file", type=str, default=None, help="Custom input file")
         parser.add_argument("-l", "--lang", type=str, default="en", help="Language to be used")
+        parser.add_argument("--tld", type=str, default="us", help="Top level domain")
         parser.add_argument("--slow", type=bool, default=False, help="Slow down the audio")
         parser.add_argument("--output", type=str, default="output.mp3", help="Output file")
         parser.add_argument("-p", "--play", type=bool, default=False)
-        parser.add_argument("-s", "--sample_rate", type=int, default=24_000)
+        parser.add_argument("-s", "--sample_rate", type=int, default=44_100)
+        parser.add_argument("-c", "--channels", type=int, default=2)
+        parser.add_argument("-r", "--record", type=bool, default=False)
+        parser.add_argument("--record_sample_rate", type=int, default=44_100)
+        parser.add_argument("--record_seconds", type=int, default=10)
 
         params: Dict = vars(parser.parse_args())
         start(params)
