8f7bcd0f635b1aac5049b282ee0b13ca5bc2d6ce
alexperezortuno <alexander.perez@sixbell.com>
2023-06-08 21:30:37 -0400

    remove bark and convert all to tts

---
+++ README.md
@@ -0,0 +1,11 @@
+
+Examples
+
+```shell
+python -m tts -t "¡Hi, how are you doing! " -p true
+python -m tts -t "¡Hola como estas! " -p true -l es
+python -m tts -t "¡Hola como estas! " -p true -l es
+python -m tts -t "¡Hola como estas! " -p true -l es --output su_num_es
+python -m tts -t "¡Hola como estas! " -p true -l es --output su_num_es -f wav
+python -m tts -t "¡Hola como estas! " -l es -s 8000 -c 1 -f wav --output su_num_es -b 160
+```
--- tts/__main__.py
+++ tts/__main__.py
@@ -1,9 +1,11 @@
-#!/usr/src/env python
 # -*- coding: utf-8 -*-
 import argparse
 import os.path
-import re
+import string
 import sys
+import subprocess
+import random
+
 import sounddevice as sd
 import soundfile as sf
 from typing import Dict
@@ -15,8 +17,12 @@ from pydub.playback import play
 def start(data: Dict):
     try:
         abs_path: str = os.path.dirname(os.path.abspath(__file__))
-        father_path: str = os.path.dirname(abs_path)
+        father_path: str = f"{os.path.dirname(abs_path)}/audio_files" if data['output_path'] == "" else data[
+            'output_path']
         input_text: str = ""
+
+        if not os.path.exists(father_path):
+            os.makedirs(father_path)
 
         if data['record']:
             my_recording = sd.rec(
@@ -38,23 +44,45 @@ def start(data: Dict):
             with open(f"{father_path}/{data['input_file']}", "r") as f:
                 input_text = f.read()
 
+        temp_name: str = random_str(20)
         tts = gTTS(text=input_text, lang=data['lang'], tld=data['tld'], slow=data['slow'])
-        tts.save(f"{father_path}/{data['output']}")
+        tts.save(f"{father_path}/{temp_name}.mp3")
 
-        file_path: str = f"{father_path}/{data['output']}"
-        _, format_ext = os.path.splitext(file_path)
+        file_path: str = f"{father_path}/{data['output']}.{data['format']}"
+        temp_file_path: str = f"{father_path}/{temp_name}.mp3"
 
-        if data['format'] != format_ext:
-            file_name = os.path.splitext(os.path.basename(data['output']))[0]
-            audio = AudioSegment.from_file(file_path, format=re.sub("[.]", "", format_ext))
-            audio.export(f"{father_path}/{file_name}.{data['format']}", format=data['format'])
-            format_ext = f".{data['format']}"
+        command: list = ["ffmpeg",
+                         "-i",
+                         temp_file_path,
+                         "-codec:a",
+                         data['codec'],
+                         "-b:a",
+                         f"{data['bit_rate']}k",
+                         "-ar",
+                         "8000",
+                         f"{file_path}",
+                         "-y"
+                         ]
+        ex = subprocess.run(command)
+
+        if ex.returncode != 0:
+            raise Exception("Error converting audio file")
+
+        os.remove(temp_file_path)
 
         if data['play']:
-            audio = AudioSegment.from_file(file_path, format=re.sub("[.]", "", format_ext))
+            audio = AudioSegment.from_file(file_path, format=data['format'],
+                                           frame_rate=data['sample_rate'],
+                                           channels=data['channels'])
             play(audio)
     except Exception as ex:
         raise ex
+
+
+def random_str(length: int) -> str:
+    c: str = string.ascii_letters + string.digits
+    r: str = ''.join(random.choice(c) for i in range(length))
+    return r
 
 
 if __name__ == '__main__':
@@ -67,18 +95,21 @@ if __name__ == '__main__':
         parser.add_argument("-t", "--text", type=str,
                             default=None,
                             help="Text to be turned into audio")
-        parser.add_argument("-i", "--input_file", type=str, default=None, help="Custom input file")
+        parser.add_argument("-i", "--input-file", type=str, default=None, help="Custom input file")
         parser.add_argument("-l", "--lang", type=str, default="en", help="Language to be used")
         parser.add_argument("--tld", type=str, default="us", help="Top level domain")
         parser.add_argument("--slow", type=bool, default=False, help="Slow down the audio")
-        parser.add_argument("--output", type=str, default="output.mp3", help="Output file")
+        parser.add_argument("--output", type=str, default="output", help="Output file")
+        parser.add_argument("--output-path", type=str, default="", help="Output folder")
         parser.add_argument("-p", "--play", type=bool, default=False)
         parser.add_argument("-f", "--format", type=str, default="mp3")
-        parser.add_argument("-s", "--sample_rate", type=int, default=44_100)
+        parser.add_argument("-s", "--sample-rate", type=int, default=44_100)
+        parser.add_argument("-b", "--bit-rate", type=int, default=32)
+        parser.add_argument("--codec", type=str, default="libmp3lame")
         parser.add_argument("-c", "--channels", type=int, default=2)
         parser.add_argument("-r", "--record", type=bool, default=False)
-        parser.add_argument("--record_sample_rate", type=int, default=44_100)
-        parser.add_argument("--record_seconds", type=int, default=10)
+        parser.add_argument("--record-sample_rate", type=int, default=44_100)
+        parser.add_argument("--record-seconds", type=int, default=10)
 
         params: Dict = vars(parser.parse_args())
         start(params)
