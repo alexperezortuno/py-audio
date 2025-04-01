7b202bf4050b0afbd4ed74cecd5312eb11d0f344
aperezortuno <alexander.perezortuno@gmail.com>
2025-04-01 12:19:40 -0300

    add deepseek

--- README.md
+++ README.md
@@ -32,10 +32,22 @@ python -m tts -t "¡Hola como estas! " -l es -s 8000 -c 1 -f wav --output su_num
 
 ---
 
-### Usage STT
+### Usage Translator
 
 declare the environment variable `OPENAI_API_KEY`
+declare the environment variable `DEEPSEEK_API_KEY`
 
 ```shell
 python -m translator
 ```
+
+```shell
+python -m translator --service openai --language es  # Usa OpenAI
+python -m translator --service deepseek --language es  # Usa DeepSeek
+```
+
+```shell
+python -m translator --service deepseek --language es -t "Am I wasting my life on a theory that can never be proven? Maybe, but how great is Game of Thrones? "
+```
+
+
--- translator/__main__.py
+++ translator/__main__.py
@@ -2,7 +2,9 @@
 import argparse
 import sys
 import queue
+import os
 
+import requests
 import numpy as np
 from enum import Enum
 from openai import OpenAI
@@ -10,11 +12,11 @@ import scipy.io.wavfile as wav
 import sounddevice as sd
 from typing import Dict
 
+
 q = queue.Queue()
-client = OpenAI()
 samplerate = 16000
 channels = 1
-duration = 5 # seconds per recording
+duration = 5  # seconds per recording
 chunk = 1024
 
 
@@ -28,10 +30,11 @@ class TranslationPrompts(Enum):
 def callback(indata, frames, time, status):
     q.put(indata.copy())
 
-def transcribe_audio(params: Dict, file_path: str) -> None:
+
+def transcribe_audio(file_path: str) -> str | None:
     """Transcribe audio using OpenAI's Whisper model."""
     try:
-        language_code = str(params["language"]).upper()
+        client = OpenAI()
         with open(file_path, "rb") as audio_file:
             transcript_response = client.audio.transcriptions.create(
                 model="whisper-1",
@@ -41,45 +44,108 @@ def transcribe_audio(params: Dict, file_path: str) -> None:
             transcript = transcript_response.strip()
             print("\n[Texto]:", transcript)
 
-            # Traducción
-            translation_response = client.chat.completions.create(
-                model="gpt-4",
-                messages=[
-                    {"role": "system", "content": TranslationPrompts[language_code].value},
-                    {"role": "user", "content": transcript}
-                ],
-                temperature=0.5
-            )
-            translation = translation_response.choices[0].message.content.strip()
-            print("[Traducción]:", translation)
+        return transcript
     except Exception as e:
         print(f"Error transcribing audio: {e}")
+        return None
 
 
-def start(params: Dict) -> None:
+def start(params_record: Dict) -> None:
     """
     Record audio from the microphone and save it to a file until user stop with CTRL + D.
-    :param params:
+    :param params_record:
     :return:
     """
-    output_file = params.get('output_file', 'output.wav')
-    language = params.get('language', 'en')
+    output_file: str = params_record.get('output_file', 'output.wav')
+    language: str = params_record.get('language', 'es').upper()
+    service: str = params_record.get('service', 'deepseek').lower()
+    text: str = params_record.get('text')
     audio_data = []
 
-    with sd.InputStream(samplerate=samplerate, channels=channels, callback=callback):
-        print("Press Ctrl+C to stop recording...")
+    if text != "":
+        t = text
+    else:
+        with sd.InputStream(samplerate=samplerate, channels=channels, callback=callback):
+            print("Press Ctrl+C to stop recording...")
 
-        try:
-            while True:
-                data = q.get()
-                audio_data.append(data)
-        except KeyboardInterrupt:
-            print("\nRecording stopped.")
-        audio_data = np.concatenate(audio_data, axis=0)
-        wav.write(output_file, samplerate, audio_data)
-        print(f"Audio saved to {output_file}")
-    transcribe_audio(params, output_file)
-    #transcribe_audio(params, "output.wav")
+            try:
+                while True:
+                    data = q.get()
+                    audio_data.append(data)
+            except KeyboardInterrupt:
+                print("\nRecording stopped.")
+            audio_data = np.concatenate(audio_data, axis=0)
+            wav.write(output_file, samplerate, audio_data)
+            print(f"Audio saved to {output_file}")
+        t = transcribe_audio(output_file)
+        t = transcribe_audio("output.wav")
+
+    if t is None:
+        print("transcription failed")
+        pass
+
+    if service == "deepseek":
+        translate_with_deepseek(t, language)
+    if service == "openai":
+        translate_with_openai(t, language)
+    if service == "ollama":
+        # TODO: implement ollama connection
+        pass
+
+
+def translate_with_deepseek(text: str, target_language: str) -> str:
+    """
+    Translate with DeepSeek
+    :param text:
+    :param target_language:
+    :return:
+    """
+    api_key = os.getenv("DEEPSEEK_API_KEY")  # Reemplaza con tu API key
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "model": "deepseek-chat",
+        "messages": [
+            {"role": "system", "content": TranslationPrompts[target_language].value},
+            {"role": "user", "content": text}
+        ],
+        "stream": False
+    }
+    response = requests.post("https://api.deepseek.com/chat/completions", headers=headers, json=data)
+    if response.status_code == 200:
+        translation: str = response.json().get("choices")[0]["message"]["content"]
+        print("[translate]:", translation)
+        return translation
+    else:
+        print(f"[ERROR]: {response.text}")
+        return text  # Fallback al texto original si hay error
+
+
+def translate_with_openai(text: str, target_language: str) -> str:
+    """
+    Translate text with OpenAI
+    :param text:
+    :param target_language:
+    :return:
+    """
+    try:
+        client = OpenAI()
+        translation_response = client.chat.completions.create(
+            model="gpt-4",
+            messages=[
+                {"role": "system", "content": TranslationPrompts[target_language].value},
+                {"role": "user", "content": text}
+            ],
+            temperature=0.5
+        )
+        translation = translation_response.choices[0].message.content.strip()
+        print("[translate]:", translation)
+        return translation.lower()
+    except Exception as e:
+        print(f"[ERROR]: {e}")
+        return text
 
 
 if __name__ == '__main__':
@@ -91,6 +157,13 @@ if __name__ == '__main__':
         parser.add_argument("-v", "--version", action="version")
         parser.add_argument("-o", "--output-file", type=str, default="output.wav", help="Output file name")
         parser.add_argument("-l", "--language", default="es", type=str, help="Language to be used")
+        parser.add_argument("-s", "--service", choices=["openai", "deepseek", "ollama"], default="deepseek",
+                            help="Service to use")
+        parser.add_argument("-t", "--text", default="", type=str, help="Translate text directly")
+        parser.add_argument("--samplerate", default=16000, type=int, help="Sampling rate")
+        parser.add_argument("--channels", default=1, type=int, help="Channels")
+        parser.add_argument("--chunk", default=1024, type=int, help="Chunk size")
+        parser.add_argument("--duration", default=5, type=int, help="Duration")
         params: Dict = vars(parser.parse_args())
         start(params)
     except KeyboardInterrupt:
